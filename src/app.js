7a428ebe580f8313f40eeb270343574e7df03530
t-rang <tranhuyentrang1806@gmail.com>
2021-07-18 23:09:47 +0700

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/hethongweb', {
@@ -14,8 +16,7 @@ require('./models/user.model');
 require('./models/course.model');
 
 const routers = require('./routers');
-app.use(routers);
-
+app.use(routers)
 app.listen(3000, () => {
 	console.log('Hi.')
 });
